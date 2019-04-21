cb0d67ea653419b50e98e8fd078974d628cafd8b
Frank Guowei Yang <frankhome61@hotmail.com>
2019-04-21 16:56:19 -0700

    added more preprocessing files

--- Code/read_data.py
+++ Code/read_data.py
@@ -1,3 +1,2 @@
-from scipy import io
-
-data = io.loadmat("imagenet-vgg-verydeep-19.mat")
+for i in range(1, 36):
+	print("./pathtracer -t 8 -l 4 -s 64 -m 8 -c ../cam/dragon-gif/{}.txt -f dragon_{}.png ../dae/sky/CBdragon_microfacet_au.dae".format(i, i))
